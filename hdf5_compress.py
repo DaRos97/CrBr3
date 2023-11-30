f5e472e10304dee77f203ad4eb805efeb378ffca
DaRos97 <dario.rossim@gmail.com>
2023-11-30 13:17:08 +0100

    faster hdf5_compress which adds to the existing file the missing datasets

--- hdf5_compress.py
+++ hdf5_compress.py
@@ -6,13 +6,14 @@ from pathlib import Path
 cluster = False if os.getcwd()[6:11]=='dario' else True
 
 #Open h5py File
-f = h5py.File(fs.name_dir_phi(cluster)[:-1]+'.hdf5','w')    #same name as folder but .hdf5
+f = h5py.File(fs.name_dir_phi(cluster)[:-1]+'.hdf5','a')    #same name as folder but .hdf5
 
 #List elements in directory
 for filename in Path(fs.name_dir_phi(cluster)).iterdir():
     filename = str(filename)
     ds_name = filename[len(filename)-filename[::-1].index('/'):-4]
-    f.create_dataset(ds_name,data=np.load(filename))
+    if ds_name not in f.keys():
+        f.create_dataset(ds_name,data=np.load(filename))
 
 
 
